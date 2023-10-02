0a21f99051fba0113aab5f0ebf85e0007735f224
dwnorm2 <dwnorm2@gmail.com>
2023-10-02 11:53:44 -0500

    finish class 20

--- class20-materials /review-summer-games/js/main.js
+++ class20-materials /review-summer-games/js/main.js
@@ -1,1 +1,9 @@
-//Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+//Create a function that takes in an array of numbers. Multiply each number together and alert the product.
+
+function multNums(num) {
+  let product = 1;
+  num.forEach((x) => (product *= x));
+  alert(product);
+}
+
+multNums([5, 2, 6]);
