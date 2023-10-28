ed8339b86f226a3b21a7bed43046c721a70da79e
dwnorm2 <dwnorm2@gmail.com>
2023-10-28 16:02:46 -0500

    zeo

--- class21-materials/objects-zeo/js/main.js
+++ class21-materials/objects-zeo/js/main.js
@@ -1,1 +1,26 @@
 //Create a stopwatch object that has four properties and three methods
+let stopWatch = {};
+stopWatch.currentTime = 3;
+stopWatch.color = "red";
+stopWatch.shape = "circle";
+stopWatch.timeZone = "CST";
+
+stopWatch.tellTime = function () {
+  console.log(`time is ${stopWatch.currentTime}`);
+};
+
+stopWatch.tellTime();
+
+stopWatch.showTimeZone = function () {
+  console.log(`time zone is ${stopWatch.timeZone}`);
+};
+
+stopWatch.showTimeZone();
+
+stopWatch.add5Seconds = function () {
+  stopWatch.currentTime += 5;
+  console.log(stopWatch.currentTime);
+};
+
+stopWatch.add5Seconds();
+stopWatch.currentTime;
