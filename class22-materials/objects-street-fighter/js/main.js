273923cd7fec086796d90f26cb4fa9568b0a2cf9
dwnorm2 <dwnorm2@gmail.com>
2023-11-05 16:17:26 -0600

    street fighter constructor

--- class22-materials/objects-street-fighter/js/main.js
+++ class22-materials/objects-street-fighter/js/main.js
@@ -1,1 +1,21 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+
+function MakeFighter(fighterStyle, fighterGender, fighterSize, fighterName) {
+  this.style = fighterStyle;
+  this.gender = fighterGender;
+  this.size = fighterSize;
+  this.name = fighterName;
+
+  this.attack = function () {
+    console.log(`${this.name} attacked!`);
+  };
+
+  this.defend = function () {
+    console.log(`${this.name} defended!`);
+  };
+
+  this.changeName = function (newName) {
+    this.name = newName;
+    console.log(`new fighter name is ${this.name}`);
+  };
+}
