1bea015ae87169f656302e1e2f7f7082779e2a02
dwnorm2 <dwnorm2@gmail.com>
2023-11-05 15:35:01 -0600

    lost galaxy

--- class22-materials/objects-lost-galaxy/js/main.js
+++ class22-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,17 @@
 //Create a mouse object that has four properties and three methods
+let mouse = {};
+mouse.color = "white";
+mouse.tailLength = "small";
+mouse.size = "large";
+mouse.weight = 10;
+
+mouse.squeak = function (squeakSound) {
+  console.log(`${squeakSound}!!!!`);
+};
+mouse.growTail = function (growBy) {
+  mouse.tailLength += growBy;
+};
+
+mouse.eat = function (numberOfLbsToEat) {
+  mouse.weight += numberOfLbsToEat;
+};
