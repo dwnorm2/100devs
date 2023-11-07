ce2b3d8f1fc1f1f0a105a3990db5f9123223f6ba
dwnorm2 <dwnorm2@gmail.com>
2023-11-07 15:37:58 -0600

    finish OOP video

--- class22-materials/OOP Youtube/OOP-ProgrammingWithMosh.js
+++ class22-materials/OOP Youtube/OOP-ProgrammingWithMosh.js
@@ -1,29 +1,47 @@
-// const circle = {
-//   radius: 1,
-//   location: {
-//     x: 1,
-//     y: 1,
-//   },
-//   draw: () => console.log("draw"),
-// };
+function Stopwatch() {
+  //private variables
+  let startTime,
+    endTime,
+    running,
+    duration = 0;
 
-// circle.draw();
+  this.start = function () {
+    if (running) {
+      throw new Error("SW already started");
+    }
 
-function createCircle(radius) {
-  return {
-    // radius: radius, (this is old way)
-    radius, //new way of saying radius: radius
-    draw: () => console.log("draw"),
+    running = true;
+
+    startTime = new Date();
   };
+
+  this.stop = function () {
+    if (!running) {
+      throw new Error("SW has not started");
+    }
+
+    running = false;
+
+    endTime = new Date();
+
+    const seconds = (endTime.getTime() - startTime.getTime()) / 1000;
+    //divided by 1000 to convert ms to seconds
+    duration += seconds;
+  };
+
+  this.reset = function () {
+    startTime = null;
+    endTime = null;
+    running = false;
+    duration = 0;
+  };
+
+  //read only property
+  Object.defineProperty(this, "duration", {
+    get: function () {
+      return duration;
+    },
+  });
 }
 
-const circle = createCircle(1);
-
-//constructor Function
-function Circle(radius) {
-  this.radius = radius;
-  this.draw = () => onsole.log("draw");
-}
-
-const another = new Circle(1);
-//new creates empty object and sets "this" to point to objcet
+const sw = new Stopwatch();
