0974c51843969e59255f3ea2abc797c28839f119
Dan <dwnorm2@gmail.com>
2023-11-19 14:56:34 -0600

    tony hawk

--- class23-materials/objects-tony-hawk-pro-skater/js/main.js
+++ class23-materials/objects-tony-hawk-pro-skater/js/main.js
@@ -1,1 +1,22 @@
-//Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
+//Create a Tony Hawk Pro Skater constructor that makes a skating game characters with 4 properties and 3 methods
+
+function CreateSkater(name, style, city, helmet) {
+  this.name = name;
+  this.style = style;
+  this.city = city;
+  this.helmet = helmet;
+
+  this.ollie = function () {
+    console.log(`${this.name} does an ollie!`);
+  };
+
+  this.takeOffHelmet = function () {
+    this.helmet = 'no';
+  };
+
+  this.kickflip = function () {
+    console.log(`${this.name} does a kickflip!`);
+  };
+}
+
+let bob = new CreateSkater('bob', 'crazy', 'chicago', 'yes');
