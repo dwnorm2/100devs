9f35f3eb51a215e11dcd3264a89d1055525a2d04
Dan <dwnorm2@gmail.com>
2023-11-26 18:08:26 -0600

    objects-lost-galaxy

--- class26-materials/objects-lost-galaxy/js/main.js
+++ class26-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,21 @@
-//Create a dog object that has four properties and three methods
+//Create a pizza object that has four properties and three methods
+
+let pizza = {}
+
+pizza.crust = "deep-dish"
+pizza.toppings = ["mushrooms", "onions", "green peppers"]
+pizza.shape = "circle"
+pizza.size = 16
+
+pizza.addTopping = function (topping) {
+    pizza.toppings.push(topping)
+}
+
+pizza.bake = function () {
+    console.log("pizza is cooked")
+}
+
+pizza.eat = function () {
+    console.log("yum!")
+}
+
