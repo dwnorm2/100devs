c24e7aec48274fadc2672cd4ea4669d1ea367ad8
Dan <dwnorm2@gmail.com>
2023-12-05 13:21:04 -0600

    add conditions

--- class26-materials/recess-weather/js/main.js
+++ class26-materials/recess-weather/js/main.js
@@ -1,44 +1,11 @@
 // //Change true/false at end to show more/less data
 // //for weather icons: https://developer.accuweather.com/weather-icons
 // //show weathertext and weathericon
-// let realFeel;
 
-// function weather() {
-//   const apiKey = 'FTr7D9KwDJzIbFpLNTVAJyWnAhMMfb4H'; // Update with your actual API key
-
-//   fetch(
-//     `http://dataservice.accuweather.com/currentconditions/v1/332667?apikey=${apiKey}&language=en-US&details=true`
-//   )
-//     .then((response) => {
-//       if (!response.ok) {
-//         throw new Error(`HTTP error! Status: ${response.status}`);
-//       }
-//       return response.json();
-//     })
-//     .then((data) => {
-//       console.log(data[0]);
-//       document.getElementById('temp').textContent =
-//         data[0].Temperature.Imperial.Value + 'f';
-//       document.getElementById('precipitation').textContent =
-//         data[0].HasPrecipitation;
-//       document.getElementById('RealFeel').textContent = `${
-//         data[0].RealFeelTemperature.Imperial.Value
-//       }f (${data[0].RealFeelTemperature.Imperial.Phrase.toLowerCase()})`;
-//       realFeel = data[0].RealFeelTemperature.Imperial.Value;
-//     })
-//     .catch((err) => {
-//       console.error(`Error: ${err.message}`);
-//     });
-// }
-
-// weather();
-// console.log(realFeel); // This will log undefined because it's executed before the fetch completes
-
-let realFeel;
-let precipitation;
+let realFeel, precipitation;
 
 function weather() {
-  const apiKey = 'FTr7D9KwDJzIbFpLNTVAJyWnAhMMfb4H'; // Update with your actual API key
+  const apiKey = 'FTr7D9KwDJzIbFpLNTVAJyWnAhMMfb4H';
 
   return new Promise((resolve, reject) => {
     fetch(
@@ -72,16 +39,17 @@ function weather() {
 
 let body = document.querySelector('body');
 
-// Usage
 weather()
   .then(({ realFeel, precipitation }) => {
-    console.log(precipitation);
-    if (precipitation === false) {
-      body.style.backgroundColor = 'blue';
+    // precipitation = true;
+    // realFeel = 95;
+    if (realFeel >= 90 || realFeel < 30) {
+      body.style.backgroundColor = 'yellow';
     }
-    // You can use both realFeel and precipitation here
+    if (precipitation === true || realFeel < 9 || realFeel > 95) {
+      body.style.backgroundColor = 'red';
+    }
   })
   .catch((error) => {
-    // Handle errors if needed
     console.error('Error:', error);
   });
