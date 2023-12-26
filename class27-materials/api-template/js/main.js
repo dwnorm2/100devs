566da2010ee29f9c9f76e99caa5dabe1acf30787
Dan <dwnorm2@gmail.com>
2023-12-26 16:29:36 -0600

    NASA POD

--- class27-materials/api-template/js/main.js
+++ class27-materials/api-template/js/main.js
@@ -1,17 +1,35 @@
 //Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector("button").addEventListener("click", getFetch);
+let todaysDate = new Date().toISOString().slice(0, 10);
+document.querySelector("input").value = todaysDate;
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  const url = 'https://pokeapi.co/api/v2/pokemon/'+choice
+function getFetch() {
+  const choice = document.querySelector("input").value;
+  if (choice > todaysDate) {
+    choice = todaysDate;
+  }
+  const key = "37xtVRoiW2VLjd3aE8UMcan0svzrKxkmlZWSXgDI";
+  const url = `https://api.nasa.gov/planetary/apod?api_key=${key}&date=${choice}`;
 
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
+      document.querySelector("img").src = "#";
+      document.querySelector("iframe").src = "";
+      console.log(data);
+
+      data.media_type == "image"
+        ? (document.querySelector("img").src = data.hdurl) &&
+          (document.querySelector("iframe").style.display = "none")
+        : (document.querySelector("iframe").style.display = "block") &&
+          (document.querySelector("iframe").src = data.url);
+
+      document.querySelector("h2").innerText = data.title;
+      document.querySelector("h3").innerText = data.explanation;
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
 }
 
+getFetch();
