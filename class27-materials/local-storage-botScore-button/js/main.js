8f6a2799fafe273b57508658e9593c522c443d46
Dan <dwnorm2@gmail.com>
2023-12-28 17:10:06 -0600

    complete botscore

--- class27-materials/local-storage-botScore-button/js/main.js
+++ class27-materials/local-storage-botScore-button/js/main.js
@@ -1,11 +1,15 @@
 //Create a button that adds 1 to a botScore stored in localStorage
 
-localStorage.setItem("botScore", 0);
+if (!localStorage.getItem("botScore")) {
+  localStorage.setItem("botScore", 0);
+}
 
 document.querySelector("button").addEventListener("click", addAnothaOne);
 
 function addAnothaOne() {
-  let botScoreVal = localStorage.getItem("botScore");
+  let botScoreVal = Number(localStorage.getItem("botScore"));
+  //add 1 to a botScore Val
   botScoreVal += 1;
+  //set the botscore in local storage to the updated botscore value
   localStorage.setItem("botScore", botScoreVal);
 }
