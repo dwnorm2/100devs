b45f9d9b2a4be2e208170837e312d57fde590533
Dan <dwnorm2@gmail.com>
2024-01-02 16:19:28 -0600

    finish isbn tracker

--- class28-materials/book-tracker/js/main.js
+++ class28-materials/book-tracker/js/main.js
@@ -1,6 +1,8 @@
 //Example fetch using pokemonapi.co
 document.querySelector("button").addEventListener("click", getFetch);
 
+document.querySelector("h2").innerText = localStorage.getItem("books");
+
 function getFetch() {
   const isbn = document.querySelector("input").value;
 
@@ -10,6 +12,15 @@ function getFetch() {
     .then((res) => res.json()) // parse response as JSON
     .then((data) => {
       console.log(data.title);
+
+      if (!localStorage.getItem("books")) {
+        localStorage.setItem("books", data.title);
+      } else {
+        let books = localStorage.getItem("books") + " ; " + data.title;
+        localStorage.setItem("books", books);
+      }
+
+      document.querySelector("h2").innerText = localStorage.getItem("books");
     })
     .catch((err) => {
       console.log(`error ${err}`);
