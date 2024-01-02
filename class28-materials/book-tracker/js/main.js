36869d0e40467f2fcd04b900e695d151f3da08a9
Dan <dwnorm2@gmail.com>
2024-01-02 11:54:25 -0600

    create isbn book tracker

---
+++ class28-materials/book-tracker/js/main.js
@@ -0,0 +1,17 @@
+//Example fetch using pokemonapi.co
+document.querySelector("button").addEventListener("click", getFetch);
+
+function getFetch() {
+  const isbn = document.querySelector("input").value;
+
+  const url = `https://openlibrary.org/isbn/${isbn}.json`;
+
+  fetch(url)
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
+      console.log(data.title);
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
+}
