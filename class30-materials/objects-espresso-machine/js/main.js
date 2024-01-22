b4678bcd7b07c7478c5b9326c7c7ff5778ceca7b
Dan <dwnorm2@gmail.com>
2024-01-22 15:20:00 -0600

    espressomachine

--- class30-materials/objects-espresso-machine/js/main.js
+++ class30-materials/objects-espresso-machine/js/main.js
@@ -1,1 +1,20 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+
+class EspressoMachine {
+  constructor(brand, model, size, cups) {
+    this.brand = brand;
+    this.model = model;
+    this.size = size;
+    this.cups = cups;
+  }
+
+  powerOn() {
+    console.log("powering on...");
+  }
+  brew() {
+    console.log("brewing...");
+  }
+  powerOff() {
+    console.log("poweringOff...");
+  }
+}
