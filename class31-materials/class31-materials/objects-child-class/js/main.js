feba29af92488cca08dda3ae6a9f8e9cf9bed6df
Dan <dwnorm2@gmail.com>
2024-02-02 14:39:57 -0600

    objects-child-class

--- class31-materials/class31-materials/objects-child-class/js/main.js
+++ class31-materials/class31-materials/objects-child-class/js/main.js
@@ -1,2 +1,33 @@
-//Create an a class and extend it - Can be anything you would like it to be! 
+//Create an a class and extend it - Can be anything you would like it to be!
 
+class Animal {
+  constructor(name) {
+    this._name = name;
+  }
+  get name() {
+    return this._name;
+  }
+  speak() {
+    console.log(`${this._name} makes a sound`);
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, breed, size) {
+    super(name);
+    this.breed = breed;
+    this.size = size;
+  }
+}
+
+class Cat extends Dog {
+  constructor(name, breed, size) {
+    super(name, breed, size);
+  }
+}
+
+let pepper = new Dog("Pepper", "ALD", "Medium");
+pepper;
+
+let luca = new Cat("Luca", "Crazy", "Small");
+luca;
