070ddc7de1fef134cfdb505fe11c80fa65759a01
Dan <dwnorm2@gmail.com>
2024-02-03 15:07:39 -0600

    comment and re-type

--- class31-materials/class31-materials/complete/js/main.js
+++ class31-materials/class31-materials/complete/js/main.js
@@ -1,47 +1,68 @@
-class Animal{
-    constructor(name){
-        this._name = name
-    }
-    get name(){
-        return this._name
-    }
-    speak(){
-        console.log(`${this._name} makes a sound`)
-    }
-}
-class Dog extends Animal{
-    constructor(name,breed){
-        super(name)
-        this._breed = breed
-    }
-    get breed(){
-        return this._breed
-    }
-    speak(){
-        super.speak()
-        console.log(`${this.name} barks`)
-    }    
-}
-class Cat extends Animal{
-    constructor(name,breed){
-        super(name)
-        this._breed = breed
-    }
-    get breed(){
-        return this._breed
-    }
-    speak(){
-        super.speak()
-        console.log(`${this.name} meows`)
-    }    
+// class Animal{
+//     constructor(name){
+//         this._name = name
+//     }
+//     get name(){
+//         return this._name
+//     }
+//     speak(){
+//         console.log(`${this._name} makes a sound`)
+//     }
+// }
+// class Dog extends Animal{
+//     constructor(name,breed){
+//         super(name)
+//         this._breed = breed
+//     }
+//     get breed(){
+//         return this._breed
+//     }
+//     speak(){
+//         super.speak()
+//         console.log(`${this.name} barks`)
+//     }
+// }
+// class Cat extends Animal{
+//     constructor(name,breed){
+//         super(name)
+//         this._breed = breed
+//     }
+//     get breed(){
+//         return this._breed
+//     }
+//     speak(){
+//         super.speak()
+//         console.log(`${this.name} meows`)
+//     }
+// }
+
+// let simba = new Dog('Simba','Shepard')
+// let machi = new Dog('The Machine','Pitbull')
+// let salem = new Cat('Salem', 'American Shorthair')
+
+// let farm = [simba,machi,salem]
+
+// for( a of farm ){
+//     a.speak()
+// }
+
+class Animal {
+  constructor(name) {
+    this._name = name;
+  }
+  //need getter in order to use _name
+  get name() {
+    //all .name does is return the property. You can't update using this.name
+    return this._name;
+  }
+  speak() {
+    console.log(`${this._name} makes a sound`);
+  }
 }
 
-let simba = new Dog('Simba','Shepard')
-let machi = new Dog('The Machine','Pitbull')
-let salem = new Cat('Salem', 'American Shorthair')
-
-let farm = [simba,machi,salem]
-
-for( a of farm ){
-    a.speak()
+class Dog extends Animal {
+  constructor(name, breed) {
+    super(name);
+    this.breed = breed;
+  }
 }
