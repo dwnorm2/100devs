f867b26a8ca0b0c797a315004fffb88a1e49c853
dwnorm2 <dwnorm2@gmail.com>
2024-03-08 13:29:43 -0600

    age-checker

--- super-review/age-checker/js/main.js
+++ super-review/age-checker/js/main.js
@@ -6,8 +6,43 @@
 //If under 30, tell them they can not rent fancy cars affordably
 //If under over 30, tell them there is nothing left to look forward too
 
+let age = 28;
+
+age < 16
+  ? console.log("no drive")
+  : age < 18
+  ? console.log("no hate")
+  : age < 21
+  ? console.log("no drink")
+  : age < 25
+  ? console.log("no rent car")
+  : age < 30
+  ? console.log("no rent fancy car")
+  : console.log("nothing left");
 
 //--- Harder
 //On click of the h1
+
+document.querySelector("h1").addEventListener("click", getValue);
+
+function ageCheckDOM(age) {
+  return age < 16
+    ? "no drive"
+    : age < 18
+    ? "no hate"
+    : age < 21
+    ? "no drink"
+    : age < 25
+    ? "no rent car"
+    : age < 30
+    ? "no rent fancy car"
+    : "nothing left";
+}
+
+function getValue() {
+  let value = document.querySelector("input").value;
+
+  document.querySelector("p").textContent = ageCheckDOM(value);
+}
 //Take the value from the input
 //Place the result of the conditional in the paragraph
