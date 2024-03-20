a7ece70a08066cbcd7932c14f6ac59f0193897e3
dwnorm2 <dwnorm2@gmail.com>
2024-03-20 11:32:03 -0500

    21 savage

--- super-review/sir-savage-the-21st/js/main.js
+++ super-review/sir-savage-the-21st/js/main.js
@@ -1,2 +1,19 @@
 //Create a function that has a loop that prints '21' 21 times to the console and then call that function
+
+function loop21() {
+  for (let i = 0; i < 21; i++) {
+    console.log(21);
+  }
+}
+
 //Bonus can you make it print '21' 21 times to the dom?
+let h2 = document.querySelector("h2");
+
+function loop21DOM() {
+  for (let i = 0; i < 21; i++) {
+    h2.textContent += "21";
+  }
+}
+
+loop21();
+loop21DOM();
