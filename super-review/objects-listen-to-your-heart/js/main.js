b1d50ab74175c24bb56ece25863e0250638fe324
dwnorm2 <dwnorm2@gmail.com>
2024-03-25 13:52:17 -0500

    objects-listen-to-your-heart

--- super-review/objects-listen-to-your-heart/js/main.js
+++ super-review/objects-listen-to-your-heart/js/main.js
@@ -1,1 +1,23 @@
 //Create a stopwatch object that has four properties and three methods
+let stopwatch = {};
+
+stopwatch.color = "black";
+stopwatch.size = "large";
+stopwatch.time = 0;
+stopwatch.brand = "coolBrand";
+
+stopwatch.tellTime = function () {
+  console.log(`Current time: ${stopwatch.time}`);
+};
+
+stopwatch.addTime = function (time) {
+  stopwatch.time += time;
+};
+
+stopwatch.subTime = function (time) {
+  stopwatch.time -= time;
+};
+
+stopwatch.addTime(10);
+stopwatch.subTime(3);
+stopwatch.tellTime();
