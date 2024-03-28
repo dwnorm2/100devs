0dc02a2985bb15db448135553118a4eced684ac6
dwnorm2 <dwnorm2@gmail.com>
2024-03-28 14:16:14 -0500

    objects-dog-photos

--- super-review/objects-dog-photos/js/main.js
+++ super-review/objects-dog-photos/js/main.js
@@ -1,1 +1,13 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
+
+let photo = document.querySelector("img");
+
+fetch("https://dog.ceo/api/breeds/image/random")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    photo.src = data.message;
+  })
+  .catch((err) => {
+    console.log(`error ${err}`);
+  });
