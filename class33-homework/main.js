6718e23a46f319e8d53889d02144ed34b2f81533
dwnorm2 <dwnorm2@gmail.com>
2024-04-10 16:14:08 -0500

    makeSmoothie()

--- class33-homework/main.js
+++ class33-homework/main.js
@@ -7,3 +7,26 @@ promise.then((res) => res.json()).then((user) => console.log(user.title));
 
 //this executes first
 console.log("synchronous");
+
+//basic
+const getFruit = async (name) => {
+  const fruits = {
+    pineapple: "hi",
+    peach: "good",
+    strawberry: "yummy",
+  };
+
+  return Promise.resolve(fruits[name]);
+};
+
+getFruit("peach").then(console.log);
+
+const makeSmoothie = async () => {
+  const a = await getFruit("pineapple");
+  const b = await getFruit("strawberry");
+  const smoothie = await Promise.all([a, b]);
+  //Promise.all tells all the promises in array to run concurrently
+  return smoothie;
+};
+
+makeSmoothie().then((smoothie) => console.log(smoothie));
